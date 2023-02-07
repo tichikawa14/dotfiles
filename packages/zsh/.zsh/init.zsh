2a9936bb44977e361e6935aeb6522a8c9c7642f0
tichikawa14 <gakusei.umeeee@gmail.com>
2023-02-07 16:19:08 +0900

    google cloud sdk追加に伴う修正

--- packages/zsh/.zsh/init.zsh
+++ packages/zsh/.zsh/init.zsh
@@ -15,3 +15,9 @@ if type brew &>/dev/null; then
   autoload -Uz compinit
   compinit
 fi
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/ichikawa/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/ichikawa/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/ichikawa/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/ichikawa/google-cloud-sdk/completion.zsh.inc'; fi
